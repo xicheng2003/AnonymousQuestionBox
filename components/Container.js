8e8d0d2f0c1a93c9ab5897082fc11c0f8de7ce0e
xicheng2003 <122506045+xicheng2003@users.noreply.github.com>
2023-01-13 21:32:26 +0800

    Update Container.js

--- components/Container.js
+++ components/Container.js
@@ -19,7 +19,7 @@ export default function Container(props) {
 
             <footer className={styles.footer}>
                 <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
-                    Powered by {"TankNee"}
+                    Powered by {"Morlight"}
                     <span className={styles.logo}>
                         <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
                     </span>
